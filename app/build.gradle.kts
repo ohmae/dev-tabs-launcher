e3ce81df129f50537a785d7a130b284a3d724a21
OHMAE Ryosuke <ryo@mm2d.net>
2020-12-16 22:17:19 +0900

    🆙kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.recyclerview:recyclerview:1.1.0")
     implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.browser:browser:1.2.0")
+    implementation("androidx.browser:browser:1.3.0")
     implementation("com.google.android.material:material:1.2.1")
     implementation("net.mm2d:color-chooser:0.2.1")
 }
