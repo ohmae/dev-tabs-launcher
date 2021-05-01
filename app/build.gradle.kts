d9bdf7a686f0840bbb5c0b3888c2aeba9bd32701
OHMAE Ryosuke <ryo@mm2d.net>
2021-05-01 23:18:49 +0900

    🆙kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.browser:browser:1.3.0")
     implementation("com.google.android.material:material:1.3.0")
-    implementation("net.mm2d.color-chooser:color-chooser:0.2.4")
+    implementation("net.mm2d.color-chooser:color-chooser:0.2.5")
 }
 
 fun isStable(version: String): Boolean {
